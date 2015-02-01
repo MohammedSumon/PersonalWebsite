5ebcdf7e0049d580f3f647ecaa1b2457e1764cb2
MohammedSumon <mrmosumon@gmail.com>
2015-02-01 00:06:10 -0500

    Sycing back deleted files

--- Comp308Portfolio/Scripts/_references.js
+++ Comp308Portfolio/Scripts/_references.js
@@ -1,4 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="lib/app.js" />
-/// <reference path="lib/bootstrap.js" />
 /// <reference path="lib/jquery.min.js" />
+/// <reference path="lib/bootstrap.min.js" />
+/// <reference path="lib/app.js" />
